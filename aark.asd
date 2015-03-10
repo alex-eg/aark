479d0839ef5dd75f2da220cbc404f907d329603f
taptap <alexey.e.egorov@gmail.com>
2015-03-10 20:15:57 +0300

    add bitmap font handling

--- aark.asd
+++ aark.asd
@@ -1,7 +1,9 @@
 (in-package #:cl-user)
 
 (asdf:defsystem :aark
-  :depends-on (:sdl2-dev)
+    :depends-on (:sdl2-dev
+                 :cffi)
   :components ((:file "packages")
                (:file "util")
+               (:file "font")
                (:file "aark")))
---
+++ font.lisp
@@ -0,0 +1,60 @@
+(in-package #:aark)
+
+(defstruct font
+  surface
+  alphabet
+  rows
+  cols
+  cell-w
+  cell-h)
+
+(define-condition load-font-failed (error)
+  ((path :initarg :path :reader path)))
+
+(defun init-font (path-to-file alphabet cell-w cell-h)
+  (cffi:with-foreign-string (foreign-string-path path-to-file)
+    (let ((font-surface (sdl2:load-bmp foreign-string-path)))
+      (if (autowrap:wrapper-null-p font-surface)
+          (error 'load-font-failed :path path-to-file))
+      (let* ((width (sdl2-ffi.accessors:sdl-surface.w font-surface))
+             (height (sdl2-ffi.accessors:sdl-surface.h font-surface))
+             (rows (floor height cell-h))
+             (cols (floor width cell-w)))
+        (make-font :surface font-surface
+                   :alphabet (loop for a across alphabet
+                                with alphabet-hash = (make-hash-table)
+                                counting a into i
+                                do (setf (gethash a alphabet-hash) (1- i))
+                                finally (return alphabet-hash))
+                   :rows rows
+                   :cols cols
+                   :cell-w cell-w
+                   :cell-h cell-h)))))
+
+(defun get-font-symbol (num font)
+  (multiple-value-bind (col row)
+      (floor num (font-cols font))
+    (sdl2:make-rect (* (font-cell-w font) row)
+                    (* (font-cell-h font) col)
+                    (font-cell-w font)
+                    (font-cell-h font))))
+
+(defmacro write-on-surface (text font dest x y
+                            &key (blit-func ''sdl2:blit-surface-scaled)
+                              (cell-w `(font-cell-w ,font))
+                              (cell-h `(font-cell-h ,font)))
+  (let ((text-list (loop for c across text
+                      collect c))
+        (char (gensym))
+        (i (gensym)))
+    `(loop for ,char in ',text-list
+        count ,char into ,i
+        do (funcall
+            ,blit-func
+            (font-surface ,font)
+            (get-font-symbol (gethash ,char (font-alphabet ,font))
+                             ,font)
+            ,dest
+            (sdl2:make-rect 
+             (+ ,x (* ,cell-w (1- ,i))) ,y
+             ,cell-w ,cell-h)))))
