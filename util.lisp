893352ab7e674fa7f54b3e9e85139845e00b0af4
taptap <alexey.e.egorov@gmail.com>
2015-03-13 16:22:28 +0300

    add fill-rect function

--- util.lisp
+++ util.lisp
@@ -11,6 +11,15 @@
   (sdl2-ffi.functions:sdl-upper-blit-scaled src srcrect
                                             dst dstrect))
 
+(defun fill-rect (surface x y w h r g b a)
+  (sdl2-ffi.functions:sdl-fill-rect
+   surface
+   (sdl2:make-rect x y w h)
+   (sdl2-ffi.functions:sdl-map-rgba
+    (sdl2-ffi.accessors:sdl-surface.format surface)
+    r g b a)))
+
+(export 'fill-rect)
 (export 'get-window-surface)
 (export 'blit-surface)
 (export 'blit-surface-scaled)
