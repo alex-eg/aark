ecdf0a8af74d710dcad39e8a032d3d91703b6722
taptap <alexey.e.egorov@gmail.com>
2015-03-22 12:22:05 +0300

    change interface of draw-sprite function

--- util.lisp
+++ util.lisp
@@ -34,13 +34,14 @@
    (sdl2-ffi.accessors:sdl-surface.w surface)
    (sdl2-ffi.accessors:sdl-surface.h surface)))
 
-(defun draw-sprite (surface sprite x y)
-  (let ((w (sdl2-ffi.accessors:sdl-surface.w sprite))
-        (h (sdl2-ffi.accessors:sdl-surface.h sprite)))
-    (sdl2:blit-surface sprite
-                       (sdl2:surface-rect sprite)
-                       surface
-                       (sdl2:make-rect x y w h))))
+(defun draw-sprite (surface sprite x y
+                    &key
+                      (w (sdl2-ffi.accessors:sdl-surface.w sprite))
+                      (h (sdl2-ffi.accessors:sdl-surface.h sprite)))
+  (sdl2:blit-surface sprite
+                     (sdl2:surface-rect sprite)
+                     surface
+                     (sdl2:make-rect x y w h)))
 
 (export 'fill-rect)
 (export 'get-window-surface)
