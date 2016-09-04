288867b253d8b1c30a7cd8c5f2b5ced023c5ae40
taptap <alexey.e.egorov@gmail.com>
2016-09-04 17:24:44 +0300

    add comments about state interface to state source

--- game.lisp
+++ game.lisp
@@ -2,6 +2,23 @@
 
 (defstruct ball
   x y dx dy)
+;;; State interface (state.lisp):
+;; (defclass state ()
+;;   ((name     :initform (error "Name must be set")
+;;              :initarg :name)
+;;    (application :initform (error "Application must be set")
+;;                 :initarg :application)
+;;    (renderer    :initform (error "Renderer must be set")
+;;                 :initarg :renderer)))
+;;
+;; (defgeneric init (state))
+;; (defgeneric process-input (state direction keysym))
+;; (defgeneric update (state))
+;; (defgeneric draw (state))
+;; (defgeneric cleanup (state))
+;;
+;; (defmethod init ((state state)))
+;; (defmethod update ((state state)))
 
 (defstruct board
   length x dx base-length r g b a)
--- menu.lisp
+++ menu.lisp
@@ -1,5 +1,23 @@
 (in-package :aark)
 
+;;; State interface (state.lisp):
+;; (defclass state ()
+;;   ((name     :initform (error "Name must be set")
+;;              :initarg :name)
+;;    (application :initform (error "Application must be set")
+;;                 :initarg :application)
+;;    (renderer    :initform (error "Renderer must be set")
+;;                 :initarg :renderer)))
+;;
+;; (defgeneric init (state))
+;; (defgeneric process-input (state direction keysym))
+;; (defgeneric update (state))
+;; (defgeneric draw (state))
+;; (defgeneric cleanup (state))
+;;
+;; (defmethod init ((state state)))
+;; (defmethod update ((state state)))
+
 ;;; Menu
 (defclass menu-state (state)
   ((choise :initform 0)))
